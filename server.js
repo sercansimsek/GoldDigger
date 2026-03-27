e21e872ac5ec9fc6247836b434be53a8e1e298b0
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-27 15:14:55 +0100

    serving the html logic divided into multiple components

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 import http from "node:http";
-import path from "node:path";
-import fs from "node:fs/promises";
+import { serveStatic } from "./utils/serveStatic.js";
 
 const __dirname = import.meta.dirname;
-const pathToResource = path.join(__dirname, "public", "index.html");
 
 const PORT = 8000;
 
 const server = http.createServer(async (req, res) => {
-	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/html");
-
-	const content = await fs.readFile(pathToResource, "utf-8");
-	res.end(content);
+	serveStatic(req, res, __dirname);
 });
 
 server.listen(PORT, () => console.log(`Server online on PORT ${PORT}`));
---
+++ utils/serveStatic.js
@@ -0,0 +1,14 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import { sendResponse } from "./sendResponse.js";
+
+export async function serveStatic(req, res, baseDir) {
+	const filePath = path.join(baseDir, "public", "index.html");
+
+	try {
+		const content = await fs.readFile(filePath);
+		sendResponse(res, 200, "text/html", content);
+	} catch (error) {
+		throw new Error(error);
+	}
+}
