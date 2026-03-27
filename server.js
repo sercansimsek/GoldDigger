6bedb859bd55e6f1606d1b4629010869ed9dbd29
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-27 14:39:59 +0100

    nodemon added

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ import http from 'node:http';
 
 const PORT = 8000;
 
+const server = http.createServer((req,res) => {
+  res.statusCode = 200;
+  res.end('Hello world!')
+})
+
+server.listen(PORT, () => `Server online on PORT ${PORT}`)
